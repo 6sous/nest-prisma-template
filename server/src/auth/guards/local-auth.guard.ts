3bf83215c76840d1a5d5f22e06ec69f046b36608
6sous <cyrille.aquilina@gmail.com>
2024-06-07 15:55:15 +0200

    update local-auth-guard for handling error after the validate method of the local strategy

--- server/src/auth/guards/local-auth.guard.ts
+++ server/src/auth/guards/local-auth.guard.ts
@@ -1,3 +1,17 @@
+import { UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  constructor() {
+    super();
+  }
+
+  handleRequest(err: any, user: any) {
+    console.log({ error: err }, { user: user });
+
+    if (err || !user) {
+      throw err || new UnauthorizedException('You should not pass!! 🥸');
+    }
+    return user;
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type', // Authorization header is not needed if using cookies
+    credentials: true, // Allow cookies to be included in cross-origin requests
+  };
+
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   await app.listen(process.env.APP_PORT || 8000);
