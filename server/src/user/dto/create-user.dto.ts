d5dc68c713b32698d11456aaab87f29171794fdc
6sous <cyrille.aquilina@gmail.com>
2024-06-07 16:18:15 +0200

    update createUserDto

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -4,6 +4,8 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  Matches,
+  MinLength,
 } from 'class-validator';
 
 export class CreateUserDto {
@@ -20,10 +22,25 @@ export class CreateUserDto {
   @IsString()
   pseudo: string;
 
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @Matches(/(?=.*[0-9])/, {
+    message: 'Password must contain at least one number',
+  })
+  @Matches(/(?=.*[a-z])/, {
+    message: 'Password must contain at least one lowercase letter',
+  })
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'Password must contain at least one uppercase letter',
+  })
+  @Matches(/(?=.*[!@#$%^&*])/, {
+    message: 'Password must contain at least one special character (!@#$%^&*)',
+  })
   password: string;
 
   refreshToken?: string = null;
